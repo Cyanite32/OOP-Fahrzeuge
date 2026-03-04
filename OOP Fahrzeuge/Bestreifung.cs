4db45231db06b5c2b9af2dacfebbc292ff675d9b
Cyanite32 <mshumail.kahn@gmail.com>
2026-03-04 14:51:41 +0100

    Fertigstellung von Bestreifung

--- OOP Fahrzeuge/Bestreifung.cs
+++ OOP Fahrzeuge/Bestreifung.cs
@@ -3,5 +3,26 @@ namespace OOP_Fahrzeuge
 {
     public class Bestreifung
     {
+        public string Marke;
+        public string Modell;
+        public enum Bauart
+        {
+            Sommer,
+            Winter
+        };
+        public Bauart bauart;
+        public int Geschwindigkeitsindex;
+        public bool Spikes;
+        public string Dimension;
+
+        public Bestreifung(string Marke, string Modell, Bauart bauart, int Geschwindigkeitsindex, bool Spikes, string Dimension)
+        {
+            this.Marke = Marke;
+            this.Modell = Modell;
+            this.bauart = bauart;
+            this.Geschwindigkeitsindex = Geschwindigkeitsindex;
+            this.Spikes = Spikes;
+            this.Dimension = Dimension;
+        }
     }
 }
